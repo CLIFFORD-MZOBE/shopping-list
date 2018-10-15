77ad75db714a37abf091ae57beff2e5c28ef2a38
siphiwe <cliffordscmzobe@gmail.com>
2018-10-15 13:31:49 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,71 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+declare var firebase;
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  product;
+  items=[];
+  list = {
+    key:'',
+    product:''
 
   }
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, ) {
+  this.getData();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  
+  //save to database(shopping) 
+  saveList(){
+    this.items=[];
+    this.list.product = this.product;
+    var database  = firebase.database();
+    database.ref('/myList/').push(this.list);
+    
+    
+  }
+  //retrieve from database(shopping)
+  getData(){
+    firebase.database().ref('myList').on("value", (snapshot) =>{
+      snapshot.forEach((snap)=>{
+        this.list.key=snap.key;
+        this.items.push({product:snap.val().product, key:snap.key});
+        return false;
+      });
+    }
+    ) 
+
+  }
+
+update(item){
+  this.list.product = this.product;
+  var database  = firebase.database();
+  database.ref('/myList/' + item.key).set(this.list);
+  this.items=[];
+  this.getData();
+}
+delete(item){
+    this.list.product = this.product;
+    var database  = firebase.database();
+    database.ref('/myList/'  + item.key).remove();
+    this.items=[];
+    this.getData();
+}
 
 }
